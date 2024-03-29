0aab9c84f9dced2f277ba1c08f8022299b05c7fb
ayaanmayooq <shaikaya@msu.edu>
2024-03-29 01:55:40 -0400

    using API to get products

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,35 @@
+"use client"
 import { cn } from "@/lib/utils"
 
+import { useState, useEffect } from 'react';
+
+import { fetchAllProducts } from "@/lib/data";
+
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchAllProducts()
+      .then(data => {
+        setProducts(data.products);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="">
       <div className="px-4 pt-20 text-center">
         <h1 className="text-4xl font-extrabold tracking-normal">Dummy E-Commerce</h1>
+        
         <p className="mx-auto mt-4 max-w-3xl text-base">Description</p>
       </div>
       <div>
         <main className="mx-auto max-w-6xl px-6">
           <div className="flex items-center justify-between border-b border-gray-200 pb-4 pt-24 dark:border-gray-800">
             <h1 className="text-xl font-bold tracking-tight sm:text-2xl">
-              0 products
+            {products.length} products
             </h1>
             {/* Product Sort */}
           </div>
---
+++ lib/data.ts
@@ -0,0 +1,13 @@
+export async function fetchAllProducts() {
+    try {
+        const response = await fetch('https://dummyjson.com/products');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        throw error;
+    }
+}
