a9da5ae0f88fb7a77e1a57208b8bdf1cf6139cdb
ayaanmayooq <shaikaya@msu.edu>
2024-03-29 01:25:32 -0400

    Lucide react components and partial header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,7 +1,31 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { Edit, ShoppingCart } from "lucide-react"
+import { useShoppingCart } from "use-shopping-cart"
+
+import { Button } from "@/components/ui/button"
+import { ThemeToggle } from "@/components/theme-toggle"
+
 export function SiteHeader() {
     return (
-        <header>
-            Header
-        </header>
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+        <div className="mx-auto flex h-16 max-w-6xl items-center justify-between space-x-4 px-6 sm:space-x-0">
+            NAVBAR
+
+            <div className="flex items-center space-x-1">
+                <Link href="/cart">
+                    <Button size="sm" variant="ghost">
+                    <ShoppingCart className="h-5 w-5" />
+                    <span className="ml-2 text-sm font-bold">0</span>
+                    <span className="sr-only">Cart</span>
+                    </Button>
+                </Link>
+                <ThemeToggle />
+            </div>
+        </div>
+
+    </header>
     )
 }
